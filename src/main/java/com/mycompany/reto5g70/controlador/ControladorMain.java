ec64b168130b185f07405c1e21f2a096df0ba6a9
girlezggm <girlesga25@gmail.com>
2022-08-13 19:11:13 -0500

    Se agrega reto 5

---
+++ src/main/java/com/mycompany/reto5g70/controlador/ControladorMain.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.reto5g70.controlador;
+
+import com.mycompany.reto5g70.vista.frmPrincipal;
+
+
+
+public class ControladorMain {
+    public static void main(String[] args) {
+        
+      new frmPrincipal().setVisible(true);  
+        
+    }
+}
